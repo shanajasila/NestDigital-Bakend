697f843db3b44ea3d7c00471d91a7b42d7f58848
shanajasila <shanajasilam@gmail.com>
2022-12-12 11:48:38 +0530

    frst

---
+++ src/main/java/com/example/NestDigital/NestDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NestDigital/NestDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NestDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
